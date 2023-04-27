7e1d7312c776aff5ad9ff83dc0212bd44713b82b
Expedite Studio <khurram.aslamm@gmail.com>
2023-04-27 12:00:02 +0500

    Initial commit

---
+++ Game/Unreal/Source/GRA_Prototype/GRA_Prototype.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "GRA_Prototype.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GRA_Prototype, "GRA_Prototype" );
